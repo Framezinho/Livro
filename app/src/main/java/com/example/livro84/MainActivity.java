21018bbc6a2f7809d840f8f8c5ec037b12bc630f
Gabriel.Z <88211315+Framezinho@users.noreply.github.com>
2024-09-18 07:54:17 -0300

    Pagina inicial
    .

---
+++ app/src/main/java/com/example/livro84/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.livro84;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
