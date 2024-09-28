dbdabef8c5e65580a22c1d92e759438a471cb112
Gabriel.Z <88211315+Framezinho@users.noreply.github.com>
2024-09-28 07:55:46 -0300

    segunda tela
    .

---
+++ app/src/main/java/com/example/livro84/Livro.java
@@ -0,0 +1,14 @@
+package com.example.livro84;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Livro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_livro);
+    }
+}
