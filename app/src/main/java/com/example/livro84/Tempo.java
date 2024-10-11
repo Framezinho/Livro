602f6ad0443ddfb5a84b15de73e6ffb184ee4b0b
Gabriel.Z <88211315+Framezinho@users.noreply.github.com>
2024-10-11 08:48:02 -0300

    Linha tempo
    .

---
+++ app/src/main/java/com/example/livro84/Tempo.java
@@ -0,0 +1,14 @@
+package com.example.livro84;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Tempo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tempo);
+    }
+}
